a75d6dadf6ccdef57c414a1d17487320e41ddcc3
Tan Lee Kent <kent.1991@live.com>
2025-02-28 21:00:05 +0800

    hall

---
+++ src/ImaxHall.java
@@ -0,0 +1,10 @@
+class ImaxHall extends Hall {
+
+    public ImaxHall(String hallName, int capacity) {
+        super(hallName, capacity);
+    }
+
+    public String getSpecialFeature() {
+        return "WIth 4D experience";
+    }
+}
