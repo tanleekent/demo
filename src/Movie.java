52b0364a1b1a1bfbfa01e5ccae53ce9d8dd46d0f
Tan Lee Kent <kent.1991@live.com>
2025-02-28 20:35:09 +0800

    add movie duration and display

--- src/Movie.java
+++ src/Movie.java
@@ -2,11 +2,13 @@ class Movie {
     private String title;
     private String genre;
     private double price;
+    private int duration; // Duration in minutes
 
-    public Movie(String title, String genre, double price) {
+    public Movie(String title, String genre, double price, int duration) {
         this.title = title;
         this.genre = genre;
         this.price = price;
+        this.duration = duration;
     }
 
     public String getTitle() {
@@ -15,5 +17,15 @@ class Movie {
 
     public double getPrice() {
         return price;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getFormattedDuration() {
+        int hours = duration / 60;
+        int minutes = duration % 60;
+        return String.format("%02d:%02d", hours, minutes);
     }
 }
