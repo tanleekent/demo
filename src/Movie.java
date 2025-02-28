e16b994dad40b9b7d370993129c5d9198bba013f
Tan Lee Kent <kent.1991@live.com>
2025-02-28 20:25:39 +0800

    added movie duration

--- src/Movie.java
+++ src/Movie.java
@@ -2,11 +2,13 @@ class Movie {
     private String title;
     private String genre;
     private double price;
+    private int duration; // Duration in minutes
 
-    public Movie(String title, String genre, double price) {
+    public Movie(String title, String genre, double price, int duration) {
         this.title = title;
         this.genre = genre;
         this.price = price;
+        this.duration = duration;
     }
 
     public String getTitle() {
@@ -15,5 +17,9 @@ class Movie {
 
     public double getPrice() {
         return price;
+    }
+
+    public int getDuration() {
+        return duration;
     }
 }
