c4eae513a427815b2f1bb8194fb5dfa5e4194954
Tan Lee Kent <kent.1991@live.com>
2025-02-28 20:24:40 +0800

    added seat number to the booking

--- src/Booking.java
+++ src/Booking.java
@@ -1,16 +1,19 @@
 class Booking {
     private Customer customer;
     private Movie movie;
+    private String seatNumber;
 
-    public Booking(Customer customer, Movie movie) {
+    public Booking(Customer customer, Movie movie, String seatNumber) {
         this.customer = customer;
         this.movie = movie;
+        this.seatNumber = seatNumber;
     }
 
     public void printBookingDetails() {
         System.out.println("Booking Details:");
         System.out.println("Customer: " + customer.getName());
         System.out.println("Movie: " + movie.getTitle());
+        System.out.println("Seat Number: " + seatNumber);
         System.out.println("Price: $" + movie.getPrice());
     }
 }
